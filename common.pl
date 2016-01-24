d282e9793c8328685d633d851dd33ca2c164d662
Fred Gleason <fredg@paravelsystems.com>
2016-01-23 23:47:32 -0500

    2016-01-22 Fred Gleason <fredg@paravelsystems.com> 	* Completed 'distill.pl'.

---
+++ common.pl
@@ -0,0 +1,154 @@
+#!/bin/perl -Wall
+
+# common.pl
+#
+# Common utility functions for the ImageForge system.
+#
+#   (C) Copyright 2016 Fred Gleason <fredg@paravelsystems.com>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License version 2 as
+#   published by the Free Software Foundation.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free Software
+#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+
+sub Prompt
+{
+    my $prompt=$_[0];
+    printf("%s (y/N)? ",$prompt);
+    my $input=lc(<STDIN>);
+    chomp $input;
+    return ($input eq "y")||($input eq "yes");
+}
+
+
+sub GetRootFilesystems
+{
+    opendir DIR, "firmware/rootfs" or die "cannot open firmware/rootfs: $!";
+    my @filesystems=readdir DIR;
+    closedir DIR;
+    return @filesystems;
+}
+
+
+sub RootFilesystemExists
+{
+    my $filesystem=$_[0];
+    my @filesystems=&GetRootFilesystems();
+    for(my $i=0;$i<@filesystems;$i++) {
+	if($filesystems[$i] eq $filesystem) {
+	    return 1;
+	}
+    }
+    return 0;
+}
+
+
+sub GetRootFilesystemSize
+{
+    $fs_name=$_[0];
+
+    my $cmd="du -s --block-size 512 firmware/rootfs/".$fs_name;
+    my @f0=split " ",`$cmd`;
+
+    return $f0[0];
+}
+
+
+sub ClearAutomounts
+{
+    my @output=`df`;
+    for(my $i=0;$i<@output;$i++) {
+	my @part=split " ",$output[$i];
+	if($part[0]=~/$config_card_reader/) {
+	    my @args=("umount",$part[0]);
+	    system @args;
+	}
+    }
+}
+
+
+sub GetImages
+{
+    opendir DIR, "firmware/images" or die "cannot open firmware/images: $!";
+    my @images=readdir DIR;
+    closedir DIR;
+    return @images;
+}
+
+
+sub ImageExists
+{
+    my $image=$_[0];
+    my @images=&GetImages();
+    for(my $i=0;$i<@images;$i++) {
+	if($images[$i] eq $image) {
+	    return 1;
+	}
+    }
+    return 0;
+}
+
+
+sub WriteImage
+{
+    my $dest=$_[0];
+    my $image=$_[1];
+
+    printf("Writing image (may take a few minutes)...");
+    my @args=("xz -cd firmware/images/".$image." | dd of=".$dest." bs=4M");
+    system @args;    
+    system "sync";
+    system "partprobe";
+    print "done.";
+}
+
+
+sub ExpandFilesystem
+{
+#
+# Get partition info
+#
+    my $cmd="parted ".$config_card_reader." unit s print";
+    my @output=`$cmd`;
+    my $size=0;
+    my $max_partnum=0;
+    my $max_start=0;
+    for(my $i=0;$i<@output;$i++) {
+	chomp($output[$i]);
+	if($output[$i] ne "") {
+	    my @f0=split " ",$output[$i];
+	    if((@f0==3)&&($f0[0] eq "Disk")&&($f0[1] eq $config_card_reader.":")) {
+		$size=substr($f0[2],0,length($f0[2])-1);
+	    }
+	    if($f0[0]=~m/\A[0-9]+\Z/) {
+		if($f0[0]>$max_partnum) {
+		    $max_partnum=$f0[0];
+		    $max_start=substr($f0[1],0,length($f0[1])-1);
+		}
+	    }
+	}
+    }
+
+#
+# Alter partition table
+#
+    system "parted ".$config_card_reader." rm ".$max_partnum;
+    system "parted ".$config_card_reader." mkpart primary ".$max_start."s ".
+	($size-1)."s";
+    system "partprobe";
+
+#
+# Resize filesystem
+#
+    system "fsck -f ".$config_card_reader.$max_partnum;
+    system "resize2fs ".$config_card_reader.$max_partnum;
+}
---
+++ distill.pl
@@ -0,0 +1,170 @@
+#!/bin/perl -Wall
+
+# distill.pl
+#
+# Create an installation image.
+#
+#   (C) Copyright 2016 Fred Gleason <fredg@paravelsystems.com>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License version 2 as
+#   published by the Free Software Foundation.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free Software
+#   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+
+do "common.pl";
+do "config.pl";
+
+sub AppendFilesystem
+{
+    my $fs_size=$_[0];
+    my @ret;
+
+    my $cmd="parted ".$config_card_reader." unit s print";
+    my @output=`$cmd`;
+    my $max_partnum=0;
+    my $max_end=0;
+    my $size;
+    for(my $i=0;$i<@output;$i++) {
+	chomp($output[$i]);
+	if($output[$i] ne "") {
+	    my @f0=split " ",$output[$i];
+	    if((@f0==3)&&($f0[0] eq "Disk")&&($f0[1] eq $config_card_reader.":")) {
+		$size=substr($f0[2],0,length($f0[2])-1);
+	    }
+	    if($f0[0]=~m/\A[0-9]+\Z/) {
+		if($f0[0]>$max_partnum) {
+		    $max_partnum=$f0[0];
+		    $max_end=substr($f0[2],0,length($f0[2])-1);
+		}
+	    }
+	}
+    }
+    system "parted ".$config_card_reader." mkpart primary ext4 ".
+	($max_end+1)."s ".($fs_size+$max_end+1)."s";
+    system "partprobe";
+    system "mkfs.ext4 -b 4096 -j ".$config_card_reader.($max_partnum+1);
+
+    $ret[0]=$config_card_reader.($max_partnum+1);
+    $ret[1]=$fs_size+$max_end+1;
+
+    return @ret;
+}
+
+
+my $USAGE="distill.pl [--list-images] [--list-roots] [output-image boot-image rootfs]";
+
+if(@ARGV==0) {
+    print $USAGE;
+    exit 256;
+}
+
+#
+# Process Options
+#
+if($ARGV[0] eq "--list-images") {
+    my @images=&GetImages();
+    for(my $i=0;$i<@images;$i++) {
+	if(substr($images[$i],-3) eq ".xz") {
+	    print $images[$i];
+	}
+    }
+    exit 0;
+}
+
+if($ARGV[0] eq "--list-roots") {
+    my @filesystems=&GetRootFilesystems();
+    for(my $i=0;$i<@filesystems;$i++) {
+	if(substr($filesystems[$i],-7) eq "-rootfs") {
+	    print $filesystems[$i];
+	}
+    }
+    exit 0;
+}
+if(@ARGV!=3) {
+    print $USAGE;
+    exit 256;
+}
+
+#
+# Check that boot image exists
+#
+if(!&ImageExists($ARGV[1])) {
+    print STDERR "distill.pl: no such image";
+    exit 256;
+}
+
+#
+# Check that root filesystems exists
+#
+if(!&RootFilesystemExists($ARGV[2])) {
+    print STDERR "distill.pl: no such rootfs";
+    exit 256;
+}
+
+#
+# Get filesystem size
+#
+my $fs_size=int(120*&GetRootFilesystemSize($ARGV[2])/100);
+
+#
+# Prompt for blank card
+#
+print "";
+print "----------------------------------------------------------";
+print " Insert blank card in reader ".$config_card_reader.".";
+print " WARNING: This will destroy any existing contents of card!";
+print "----------------------------------------------------------";
+if(!&Prompt("Proceed")) {
+    print "Operation cancelled!";
+    exit 0;
+}
+
+#
+# Undo the work of the "helpful" GNOME automounter
+#
+&ClearAutomounts();
+
+#
+# Transfer boot image
+#
+&WriteImage($config_card_reader,$ARGV[1]);
+
+#
+# Append new partition
+#
+my $newpart_name;
+my $final_size;
+($newpart_name,$final_size)=&AppendFilesystem($fs_size);
+
+#
+# Copy root filesystem
+#
+printf("Copying filesystem (may take a few minutes)...");
+system "mount ".$newpart_name." /mnt";
+system "cp -a firmware/rootfs/".$ARGV[2]."/* /mnt/";
+system "umount ".$newpart_name;
+system "sync";
+print "done.";
+
+#
+# Copy distilled image
+#
+printf("Distilling final image (may take a few minutes)...");
+system "dd if=".$config_card_reader." bs=4M count=".(int($final_size/8192)+1).
+    " | xz > firmware/images/".$ARGV[0];
+print "done.";
+
+
+print "";
+print "Done!";
+print "";
+exit 0;
